d669964e573e162d12215b8f0a73c0138bf8a658
SergUserGit <104759161+SergUserGit@users.noreply.github.com>
2023-04-03 14:32:41 +0000

    Deploying to gh-pages from @ SergUserGit/goit-react-hw-08-phonebook@b3a10b52d8ccb58693e5af4e6263f05d1d09d3be 🚀

---
+++ static/js/266.49dd495f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[266],{266:function(e,n,t){t.r(n),t.d(n,{default:function(){return c}});var o=t(184);function c(){return(0,o.jsx)("div",{children:(0,o.jsx)("h1",{children:"Contacts manager welcome page"})})}}}]);
+//# sourceMappingURL=266.49dd495f.chunk.js.map
